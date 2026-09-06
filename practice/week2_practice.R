18283904a4ae134b6fada177cacba7a958548513
CyannnZ <chenyan.zhao03@gmail.com>
2026-09-06 14:16:28 +0800

    weekly note_2

---
+++ practice/week2_practice.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(tidycensus)
+
+#I'm going to load some data
+pa_income = get_acs(
+  geography = "county",
+  variables = "B19013_001",
+  state = "PA",
+  year =2023,
+  survey = "acs5"
+)
+
+pa_income
+
+dim(pa_income)
+glimpse(pa_income)
+head(pa_income,10)
+
+pa_income$GEOID
+as.numeric("01001")
+
+filter(pa_income, estimate > 60000)
+
+select(pa_income, NAME, estimate, moe)
